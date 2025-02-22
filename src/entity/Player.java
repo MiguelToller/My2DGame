2e78be8de18dfe9ab7d0ae1c6861c0ad01705ad2
MiguelToller <luizmigueltoller@gmail.com>
2025-02-22 10:51:03 -0300

    World and Camera

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -16,10 +16,16 @@ public class Player extends Entity {
 	GamePanel gp;
 	KeyHandler keyH;
 
+	public final int screenX;
+	public final int screenY;
+	
 	public Player(GamePanel gp, KeyHandler keyH) {
 
 		this.gp = gp;
 		this.keyH = keyH;
+		
+		screenX = gp.screenWidth/2 - (gp.tileSize/2);
+		screenY = gp.screenHeight/2 - (gp.tileSize/2);
 
 		setDefaultValues();
 		getPlayerImage();
@@ -27,8 +33,8 @@ public class Player extends Entity {
 
 	public void setDefaultValues() {
 
-		x = 100;
-		y = 100;
+		worldX = gp.tileSize * 23; // player position
+		worldY = gp.tileSize * 21;
 		speed = 4;
 		direction = "right";
 	}
@@ -61,16 +67,16 @@ public class Player extends Entity {
 
 			if (keyH.upPressed == true) {
 				direction = "up";
-				y -= speed;
+				worldY -= speed;
 			} else if (keyH.downPressed == true) {
 				direction = "down";
-				y += speed;
+				worldY += speed;
 			} else if (keyH.leftPressed == true) {
 				direction = "left";
-				x -= speed;
+				worldX -= speed;
 			} else if (keyH.rightPressed == true) {
 				direction = "right";
-				x += speed;
+				worldX += speed;
 			}
 
 			spriteCounter++;
@@ -126,7 +132,7 @@ public class Player extends Entity {
 				image = right3;
 			break;
 		}
-		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+		g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
 	}
 
 }
