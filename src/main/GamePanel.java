207a654995882e4958c5d9f19454cc64d9f750d9
MiguelToller <luizmigueltoller@gmail.com>
2025-02-22 14:26:23 -0300

    Object Placement

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import object.SuperObject;
 import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
@@ -35,7 +36,10 @@ public class GamePanel extends JPanel implements Runnable {
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
 	public CollisionChecker cChecker = new CollisionChecker(this);
+	public AssetSetter aSetter = new AssetSetter(this);
 	public Player player = new Player(this, keyH);
+	public SuperObject obj[] = new SuperObject[10]; // 10 objects at the same time
+	
 
 	public GamePanel() {
 
@@ -44,6 +48,10 @@ public class GamePanel extends JPanel implements Runnable {
 		this.setDoubleBuffered(true);
 		this.addKeyListener(keyH);
 		this.setFocusable(true);
+	}
+	
+	public void setupGame() {
+		aSetter.setObject();
 	}
 
 	public void startGameThread() {
@@ -88,12 +96,20 @@ public class GamePanel extends JPanel implements Runnable {
 	}
 
 	public void paintComponent(Graphics g) {
-
 		super.paintComponent(g);
-
 		Graphics2D g2 = (Graphics2D) g;
 		
+		// TILE
 		tileM.draw(g2);
+		
+		// OBJECT
+		for (int i = 0; i < obj.length; i++) {
+			if(obj[i] != null) {
+				obj[i].draw(g2, this);
+			}
+		}
+		
+		// PLAYER
 		player.draw(g2);
 
 		g2.dispose();
