992add2853307979948a67a6db6b415034e7b581
MiguelToller <luizmigueltoller@gmail.com>
2025-02-27 13:57:19 -0300

    Monster

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.NPC_Shepheard;
+import monster.MON_GreenSlime;
 import object.OBJ_Door;
 
 public class AssetSetter {
@@ -24,5 +25,16 @@ public class AssetSetter {
 		gp.npc[0] = new NPC_Shepheard(gp);
 		gp.npc[0].worldX = gp.tileSize * 21;
 		gp.npc[0].worldY = gp.tileSize * 21;
+	}
+	
+	public void setMonster() {
+		
+		gp.monster[0] = new MON_GreenSlime(gp);
+		gp.monster[0].worldX = gp.tileSize*23;
+		gp.monster[0].worldY = gp.tileSize*36;
+		
+		gp.monster[1] = new MON_GreenSlime(gp);
+		gp.monster[1].worldX = gp.tileSize*23;
+		gp.monster[1].worldY = gp.tileSize*37;
 	}
 }
